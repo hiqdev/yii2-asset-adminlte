288f7d3799d9e34f7577da62330ae8a5a50d68b6
Andrii Vasyliev <sol@hiqdev.com>
2015-05-25 10:53:48 +0000

    inited

---
+++ AdminLteAsset.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * @link        http://hiqdev.com/yii2-asset-adminlte
+ * @license     BSD 3-clause
+ * @copyright   Copyright (c) 2015 HiQDev
+ */
+
+namespace hipanel\themes\adminlte;
+
+class AdminLteAsset extends \yii\web\AssetBundle
+{
+    public $sourcePath = '@bower/adminlte';
+
+    public $css = [
+        'dist/css/AdminLTE.css',
+        'dist/css/skins/_all-skins.css',
+    ];
+
+    public $js = [
+        'plugins/slimScroll/jquery.slimscroll.min.js',
+        'plugins/fastclick/fastclick.min.js',
+        'dist/js/app.js',
+    ];
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+Yii2 asset for bower adminlte
+-----------------------------
+
+Wrapper around bower package with copied versions
+
+For more info see wiki:
+https://github.com/hiqdev/yii2-asset-adminlte/wiki
