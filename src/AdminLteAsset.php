bbe680039d7a2ab12893f6eb58e107cff3343846
Andrii Klochok <andreyklochok@gmail.com>
2024-04-01 15:20:34 +0300

    HP-1883: use min assets only

--- src/AdminLteAsset.php
+++ src/AdminLteAsset.php
@@ -15,14 +15,14 @@ class AdminLteAsset extends \yii\web\AssetBundle
     public $sourcePath = '@bower/adminlte';
 
     public $css = [
-        'dist/css/AdminLTE.css',
-        'dist/css/skins/_all-skins.css',
+        'dist/css/AdminLTE.min.css',
+        'dist/css/skins/_all-skins.min.css',
     ];
 
     public $js = [
         'plugins/slimScroll/jquery.slimscroll.min.js',
         'plugins/fastclick/fastclick.min.js',
         'plugins/pace/pace.js',
-        'dist/js/app.js',
+        'dist/js/app.min.js',
     ];
 }
